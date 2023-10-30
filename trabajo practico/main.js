3bc494f842b305ebdb5a0136293633ababd865ea
jumimonteza <juanmonvar@hotmail.com>
2023-10-30 17:18:54 -0300

    correccion de 2da entrega

--- trabajo practico/main.js
+++ trabajo practico/main.js
@@ -2,7 +2,7 @@ let nombre = prompt("ingresa tu nombre");
 let apellido = prompt("ingresa tu apellido");
     alert(`Hola ${nombre} ${apellido}`);
 
-let noches = (prompt("ingrese el numero de dias para saber los beneficios"))
+let noches = (prompt("ingrese los dias de hospedaje para conocer los beneficios"))
 if (noches <= 2){
     alert ("sin beneficios");
 }else if(noches <= 7){
@@ -10,75 +10,45 @@ if (noches <= 2){
 }else{
     alert("tienes el desayuno y tres cenas a elección GRATIS!");
 }
-let importe = Number(prompt("confirme los dias de hospedaje"));
+let importe = Number(prompt("confirme los dias de hospedaje para calcular el importe total"));
 for(let j = 3000; j <= 3000; j++) {
     let resultado = importe * j;
     alert(`${importe} * ${j} = ${resultado}`);
 }
 
-function calcular(numeroUno, numeroDos, operacion) {
-    switch (operacion) {
-        case "+":
-            return numeroUno + numeroDos;
-            break;
-        case "*":
-            return numeroUno * numeroDos;
-            break;
-        default:
-            return "ingresó un dato erroneo";
-            break;
-    }
-}
+let saludo = prompt("Gracias por preferirnos, estas son nuestras actividades");
+function actividades() {
+let actividad = prompt("ingresa el numero de la actividad que quieres realizar 1-Trekking 2-Tirolesa");
+if(actividad === "1"){
+    alert("elegiste Trekking");
+}else if(actividad === "2"){
+    alert("elegiste Tirolesa");
+}else {
+    alert("ingresa solo el numero 1 o 2");
+    let actividad = prompt("ingresa la actividad que quieres realizar 1-Trekking 2-Tirolesa");
+}}
+actividades();
 
-let personas = Number(prompt("personas por cabaña para calcular desayunos"));
-let desayunos = Number(prompt("ingresa los dias de hospedaje (mayor a dos)"));
-let cantidad = prompt("ingresa la operación para calcular");
-
-let resultado = calcular (personas, desayunos, cantidad);
-alert(`${personas} ${cantidad} ${desayunos} = ${resultado} desayunos`); 
-
-let actividad = prompt("¿deseas info de la actividad al aire libre?");
-function actividadUno(caminata) {
-    return `salida para ${caminata}`
+const grupo = ["Eduardo", "Jose", "Natalia", "Ivana", "Vanina"];
+let nuevo = prompt("ingresa tu nombre para agregarte al grupo");
+if (grupo.push("")){ 
+alert(`agregado a la lista : ${grupo} y vos: ${nuevo}`);
 }
-function actividadDos(aves) {
-    return `${aves}, avistamiento de aves`
-}
-function actividadTres(aventura) {
-    return `${aventura}, aventura en tirolesa y almuerzo.`;
-}
-function finActividades(retorno) {
-    console.log(`${retorno} Fin de actividades al aire libre.`);
-}
-let actividadDelDia = actividadUno("trekking");
-let verAves = actividadDos(actividadDelDia);
-let actividadDeAventura = actividadTres(verAves);
-alert(actividadDeAventura);
-
-let paqueteCompleto = prompt("deseas el paquete completo?");
-const actividades = ["trekking", "avistamiento de aves", "tirolesa"];
-const almuerzo = ["entrada", "plato principal", "postre", "bebida"];
-const paquete = actividades.concat(almuerzo);
-alert(paquete);
-
-/*function otraActividad(nombre, dia, hora, valor) {
-    this.nombre = nombre;
+function Itinerario(salida, dia, hora) {
+    this.salida = salida;
     this.dia = dia;
     this.hora = hora;
-    this.valor = valor;
 }
- const actividad = new otraActividad("parapente", "martes", "16hs", 2000);
- console.log(actividad);
+const trekking = new Itinerario("ingreso al complejo de cabañas", "viernes", "10am");
+const tirolesa = new Itinerario("ingreso al complejo de cabañas", "sabados", "11am");
 
-const grupos = ["cabaña1", "cabaña2", "cabaña3"];
- grupos.push("cabaña4");
-console.log(grupos);*/
-
-
-
-
-
-
+let solicitud = prompt("Ingresa 'trekking' o `tirolesa` para conocer el itinerario");
+if (solicitud === 'trekking') {
+    alert(`Salida: ${trekking.salida}, Día: ${trekking.dia}, Hora: ${trekking.hora}`);
+} else if (solicitud === `tirolesa`) {
+    alert(`Salida: ${tirolesa.salida}, Día: ${tirolesa.dia}, Hora: ${tirolesa.hora}`);
+}else
+alert("Solicitud no válida. Por favor, ingresa 'trekking'o`tirolesa` para conocer el itinerario.");
 
 
 
